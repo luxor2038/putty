0f9879c2d49c574ee944f26ddbae136d221eb35c
luxor <luxor2012@gmail.com>
2023-10-30 17:33:57 +0800

    fix portfwdmgr_config()

--- ssh/portfwd.c
+++ ssh/portfwd.c
@@ -819,9 +819,9 @@ void portfwdmgr_config(PortFwdManager *mgr, Conf *conf)
         kp = key;
 
         address_family = 'A';
-        if(address_family == ADDRTYPE_IPV4) {
+        if(addressfamily == ADDRTYPE_IPV4) {
             address_family = '4';
-        } else if(address_family == ADDRTYPE_IPV6) {
+        } else if(addressfamily == ADDRTYPE_IPV6) {
             address_family = '6';
         }
 
